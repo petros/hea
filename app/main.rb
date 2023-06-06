dcf9fa380f1e819b7b87785ba749072f04b0d413
Petros Amoiridis <petros@amignosis.com>
2023-06-06 16:41:16 +0300

    Make Henrietta move left and right

--- app/main.rb
+++ app/main.rb
@@ -68,8 +68,164 @@ def print_high_score
   args.outputs.labels << labels
 end
 
-def game_scene(args)
+def initialize_player(args)
+  size = 64
+  args.state.player ||= {
+    x: 120,
+    y: 0,
+    w: size,
+    h: size,
+    speed: 12,
+    facing: :left,
+  }
+end
 
+def draw_background(args)
+  args.outputs.sprites << {
+    x: 0,
+    y: 0,
+    w: args.grid.w,
+    h: args.grid.h,
+    path: 'sprites/hea-game-scene-background.png'
+  }
+end
+
+def move_player(args)
+  moved = false
+  if args.inputs.left
+    args.state.player.x -= args.state.player.speed
+    moved = true
+    if args.state.player.facing != :left
+      args.state.player.flip_horizontally = false
+      args.state.player.facing = :left
+    end
+  elsif args.inputs.right
+    args.state.player.x += args.state.player.speed
+    moved = true
+    if args.state.player.facing != :right
+      args.state.player.flip_horizontally = true
+      args.state.player.facing = :right
+    end
+  end
+  # if args.inputs.up
+  #   args.state.player.y += args.state.player.speed
+  #   moved = true
+  # elsif args.inputs.down
+  #   args.state.player.y -= args.state.player.speed
+  #   moved = true
+  # end
+  if args.state.player.x + args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
+  end
+  if args.state.player.x < 0
+    args.state.player.x = 0
+  end
+  # if args.state.player.y + args.state.player.h > args.grid.h
+  #   args.state.player.y = args.grid.h - args.state.player.h
+  # end
+  # if args.state.player.y < 0
+  #   args.state.player.y = 0
+  # end
+  if moved
+    args.state.player.moved = moved
+  end
+end
+
+def animate_player(args, speed)
+  index = 0.frame_index(count: 1, hold_for: speed, repeat: true)
+  args.state.player.path = "sprites/henrietta/henrietta-#{index}.png"
+end
+
+def update_sprites(args)
+  if args.state.player.moved
+    animate_player(args, 1)
+    args.state.player.moved = false
+  else
+    animate_player(args, 3)
+  end
+  args.outputs.sprites << [args.state.player]
+end
+
+def update_labels(args)
+  labels = []
+  labels << {
+    x: 40,
+    y: args.grid.h - 40,
+    text: "Score: #{args.state.score}",
+    size_enum: 4,
+  }
+  labels << {
+    x: args.grid.w - 40,
+    y: args.grid.h - 40,
+    text: "Lives: #{args.state.lives}",
+    size_enum: 2,
+    alignment_enum: 2,
+  }
+  args.outputs.labels << labels
+end
+
+def game_scene(args)
+  initialize_player(args)
+  args.state.score ||= 0
+  args.state.lives ||= 3
+  draw_background(args)
+  if args.state.lives == 0
+    args.audio[:music].paused = true
+    args.audio[:game_over] = { input: "sounds/game-over.wav", looping: false }
+    args.state.scene = "game_over"
+    return
+  end
+  move_player(args)
+  update_sprites(args)
+  update_labels(args)
+end
+
+def game_over_scene(args)
+  args.state.timer -= 1
+
+  if !args.state.saved_high_score && args.state.score > args.state.high_score
+    args.gtk.write_file(HIGH_SCORE_FILE, args.state.score.to_s)
+    args.state.saved_high_score = true
+  end
+
+  labels = []
+  labels << {
+    x: 40,
+    y: args.grid.h - 40,
+    text: "Game Over!",
+    size_enum: 10,
+  }
+  labels << {
+    x: 40,
+    y: args.grid.h - 90,
+    text: "Score: #{args.state.score}",
+    size_enum: 4,
+  }
+  labels << {
+    x: 40,
+    y: args.grid.h - 132,
+    text: "Fire to restart",
+    size_enum: 2,
+  }
+  if args.state.score > args.state.high_score
+    labels << {
+      x: 260,
+      y: args.grid.h - 90,
+      text: "New High Score!",
+      size_enum: 3,
+    }
+  else
+    labels << {
+      x: 260,
+      y: args.grid.h - 90,
+      text: "Score to beat: #{args.state.high_score}",
+      size_enum: 3,
+    }
+  end
+  args.outputs.labels << labels
+  if args.state.timer < -30 && player_fired?(args)
+    $gtk.reset
+  end
 end
 
 def tick args
@@ -87,3 +243,5 @@ def tick args
     send("#{args.state.scene}_scene", args)
   end
 end
+
+$gtk.reset
